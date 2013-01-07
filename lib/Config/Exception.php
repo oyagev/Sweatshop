92e7eed7de6e7da368a26353b3e02db810804e06
Oren Yagev <oren@onehourtranslation.com>
2013-01-07 10:44:18 +0200

    Cleanup

--- lib/Config/Exception.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Sweatshop\Config;
-
-class Exception extends \RuntimeException{
-	
-}
--- lib/Queue/Exceptions/QueueServerUnavailableException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Sweatshop\Queue\Exceptions;
-
-class QueueServerUnavailableException extends \RuntimeException{
-	
-}
